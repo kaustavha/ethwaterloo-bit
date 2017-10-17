08f111da0cb0a19e999671ac8c2037990af19cb9
Kaustav Haldar <hi@kaustav.me>
2017-10-17 15:15:30 -0400

    add docs and second dapp

--- README.md
+++ README.md
@@ -1,37 +1,55 @@
-# Setup
+# README - Basic Identity Token
+
+Welcome to the PoC of BIT. 
+This PoC attempts to be an implementation of a "login w/ facebook" button for web3.0 dapps. Offering a bridge of sorts for transitioning to web3.0 services while maintaining the users social profile information and allowing them to identify strongly with the services vs weakly when they're being represented by an anonymous SHA3 hash, i.e. wallet address. 
+
+## SETUP
+There's 2 projects here.  
+IdentityStore provides the identity retrieval, storage abilities
+GooseHunter is an implementation of using the IdentityStore dapp and frontend for authentication.  You'll need 3 open terminal windows.  
+
+- First get testrpc running and give yourself some cash money. Replace the account id with your testrpc private key.  
 ```
-truffle compile
-truffle migrate
+ testrpc --account="0x+PrivKey,1000000000000000000"
+```
+- Get the identity provider running: 
+```
+cd IdentityStore
+npm i
+npm run run
+```
+- Run the GooseHunter auth user app
+```
+cd GooseHunter
+npm i
+truffle compile && truffle migrate
 npm start
 ```
 
-# README
 
-- ethereum prize?
- - starting talk
- - privacy on blockchain
- - ring signatures are dank
- - ring signatures on profiles? linkable ring sigs, better mixers?
- - on chain whitelists and walled elitist gardens?
- - try to use state channels/sharding/ring sigs
- - use byzantium somehow? like ring sigs?
+## Walkthrough
 
-- do somehthing with state channels?
- build a connection between blockchain and real world element, but what about real world apps?
-
- - toshi api prize
- - interview at coinbase
+Authentication flows can be one of 2:
+- User starts at GooseHunter app, which attempts to personalize its frontend based on social info. If the user does not have an acc they will be re-routed to the IdentityStore app.  
+In the IdentityStore app they can "login w/ facebook".
+Profile information from FB is written to the blockchain for a nominal fee.   
+IdentityStore then reroutes back to GooseHunter which now has profile information and can render it. 
+All future profile use is effectively free as its free to read from the blockchain.   
+This also provides a proof of identity and proof of login for the user in case its needed.   
 
 
- - 0x - simple building blocks - i make simple building block
+- Alternatively, the user can first login @ IdentityStore, and any future use of GooseHunter will be autopopulated as the profile data is looked up by the users SHA3 wallet address hash. 
 
- - tendermint - 10eth - AI - fail rap
+## Experimental featurettes:
+- taxation & identity pruning
+- identity pruning
+- Staking associated with id's to prevent abusive behaviour
 
 
- Alex Leverington ideas
- - lottery -> incentivize the login w/ fb, people who login into 2nd dapp using 1st dapp are entered into lottery, blockchain can handle payments easily and theres a paper trail of lottery tickets. 
+## PICTURES!!
 
- Steve - email idea -> stop spam by making companies pay to get my email address, but provide enough for fb login
-
- ## TESTRPC example
- `testrpc --account="0x1422ee3048ac357cd7d1afda10db1af59d70721f88c5d6aeaa556b602bf27e78,1000000000000000000"`
+![Secondary Dapp/user](./readme_images/1.png) Secondary Dapp/user
+![Authenticated BIT using app](./readme_images/2.png) Authenticated BIT using app
+![BIT PoC introscreen](./readme_images/5.png) BIT PoC introscreen
+![BIT data display](./readme_images/3.png) BIT data display
+![BIT data display 2](./readme_images/4.png) BIT data display 2
---
+++ secondDapp/GooseHunter/src/App.js
@@ -0,0 +1,228 @@
+import React, { Component } from 'react'
+import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+// import getWeb3 from './utils/getWeb3'
+import IdentityStorageContract from '../build/contracts/IdentityStorage.json'
+const contract = require('truffle-contract');
+// const ISA = contract(IdentityStorageContract);
+// const address = window.web3.eth.defaultAccount;
+import ghjson from '../build/contracts/GooseHunter.json'
+
+
+// Goosehunter bounties
+// import ghb from '../../StandardBounties/build/contracts/StandardBounties.json'
+// window.ISA = ISA;
+
+import './css/oswald.css'
+import './css/open-sans.css'
+import './css/pure-min.css'
+import './App.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+  // window.ISA.setProvider(window.web3.currentProvider);
+    this.state = {
+      money: 0,
+      web3: window.web3,
+      profile: "",
+      updated: false
+    }
+    this.auth = this.auth.bind(this);
+  }
+
+  // componentWillMount() {
+  //   // Get network provider and web3 instance.
+  //   // See utils/getWeb3 for more info.
+
+  //   // getWeb3
+  //   // .then(results => {
+  //   //   this.setState({
+  //   //     web3: results.web3
+  //   //   })
+
+  //     // Instantiate contract once web3 provided.
+  //     // this.instantiateContract()
+  //   })
+  //   .catch(() => {
+  //     console.log('Error finding web3.')
+  //   })
+  // }
+
+  instantiateContract() {
+    /*
+     * SMART CONTRACT EXAMPLE
+     *
+     * Normally these functions would be called in the context of a
+     * state management library, but for convenience I've placed them here.
+     */
+
+    const contract = require('truffle-contract')
+    const simpleStorage = contract(SimpleStorageContract)
+    simpleStorage.setProvider(this.state.web3.currentProvider)
+
+    // Declaring this for later so we can chain functions on SimpleStorage.
+    var simpleStorageInstance
+
+    // Get accounts.
+    this.state.web3.eth.getAccounts((error, accounts) => {
+      simpleStorage.deployed().then((instance) => {
+        simpleStorageInstance = instance
+        // Stores a given value, 5 by default.
+        return simpleStorageInstance.set(5, {from: accounts[0]})
+      }).then((result) => {
+        // Get the value from the contract to prove it worked.
+        return simpleStorageInstance.get.call(accounts[0])
+      }).then((result) => {
+        // Update state with the result.
+        return this.setState({ storageValue: result.c[0] })
+      });
+    });
+  }
+
+  showMoney() {
+    if (this.state.updated) {
+      return (<p> You have: $Eth {this.state.money} </p>);
+    }
+  }
+
+  showChirp(){
+    if (this.state.updated) {
+      if (this.state.money == 0) {
+        return (
+          <div>
+          <p>Wow youre broke :,( </p>
+          <p>Lets enter you into the Lottereth</p>
+          <p> And lets get you a job as a Goose Hunter</p>
+          <button onClick={()=>this.GooseHunterBounty()}>Accept Job offer</button>
+          </div>
+          );
+      } else {
+        return (<p>Hey youre rich, can I have some?</p>);
+      }
+    }
+  }
+
+  showProfile(){
+    if (this.state.profile !== undefined && this.state.profile.name !== undefined){
+      return (
+        <p>Hi there {this.state.profile.name}</p>
+        // <p> Would you like to meet our lord and saviour 
+        );
+    }
+  }
+
+  GooseHunterBounty() {
+    console.log("TODO");
+  }
+
+
+  redirectForCreds(cb) {
+    alert("Hey looks like you dont have an acc, we'll redirect you to our login provider");
+    alert(window.location.host);
+    // alert(window.ISA.currentUrl);
+    // window.ISA.setReturnPath(window.location.host).then((e,r)=>{
+    //   alert(e);
+    //   alert(r);
+    // });
+    // alert(window.ISA.currentUrl((e,r)=>{
+    //   console.log(e,r);
+    // }));
+    // window.ISA.currentUrl().then((r,e)=>{
+    //   console.log(r,e);
+    // });
+    window.location.assign("http://localhost:3000"+"?returl="+window.location.host);
+  }
+
+  auth() {
+    window.nexti = 0;
+    window.exposeForDebug = function(a,b) {
+      console.log("NEXT " + window.nexti);
+      window.a = a;
+      window.b = b;
+      console.log(a,b);
+      window.nexti++;
+    }
+
+    var self = this;
+    self.finderObj = {};
+
+    var is = contract(IdentityStorageContract);
+    is.setProvider(window.web3.currentProvider);
+
+    is.deployed().then((instance)=>{
+      window.ISA = instance;
+      var address = this.state.web3.eth.defaultAccount;
+      return window.ISA.findByAddress(address);
+    }).then((r,e)=>{
+      // convert bignum, and redirec tif -1
+      var num = r.toNumber();
+      if (r == -1){
+        //no acc
+        this.redirectForCreds(this.auth);
+      }
+      self.id = r;
+      window.exposeForDebug(r,e);
+      return window.ISA.getAll(self.id);
+    }).then((r,e)=>{
+      window.exposeForDebug(r,e);
+      self.finderObj.associatedAddress = r[1];
+      self.setState({money: r[2].toNumber()});
+      console.log(self.id);
+      return window.ISA.getSocial(self.id);
+    }).then((r,e)=>{
+      window.exposeForDebug(r,e);
+      console.log(r[0]);
+
+      self.setState({profile:{
+        name: r[0]
+      }});
+      self.setState({updated: true});
+
+      window.userProfile = r;
+      // self.showProfile();
+      console.log(r,e);
+    });
+  }
+
+  render() {
+    var acc;
+    if (this.state.web3 == undefined) {
+      if (window.web3 !== undefined) {
+        this.setState({web3: window.web3});
+      } else {
+        this.setState({web3: {eth: null}});
+      }
+    }
+    if (this.state.web3.eth.defaultAccount !== undefined) {
+      acc = this.state.web3.eth.defaultAccount;
+    } else if (window.web3 !== undefined) {
+      acc = window.web3.eth.defaultAccount;
+    } else {
+      acc = "Err";
+    }
+    return (
+      <div className="App">
+        <nav className="navbar pure-menu pure-menu-horizontal">
+            <a href="#" className="pure-menu-heading pure-menu-link">BIT Auth frontend examples</a>
+        </nav>
+
+        <main className="container">
+          <div className="pure-g">
+            <div className="pure-u-1-1">
+              <h1>Hi and welcome to the BIT auth demo!</h1>
+              <p>Lets get to know you. Right now all I know is your metamask address, which is: {acc}</p>
+
+              <button onClick={()=>this.auth()}> Lets find out who you are! </button>
+              {this.showProfile()}
+              {this.showMoney()}
+              {this.showChirp()}
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App
